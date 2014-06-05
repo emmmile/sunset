2fd3952d92f8f9ccf2c05271e1ef321257ab78e1
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2014-06-05 16:33:38 +0200

    humidity function

--- js/output.js
+++ js/output.js
@@ -1,9 +1,9 @@
 
 function output(data, situation) {
 	var el = document.getElementById("weather");
-	el.innerHTML = "Summary: <strong>" + data.currently.summary + "</strong>"
-	"<br>Cloud cover: " + (data.currently.cloudCover*100) + "%"
-	"<br>Humidity: " + (data.currently.humidity*100) + "%"
+	el.innerHTML = "Summary: <strong>" + data.currently.summary + "</strong>" +
+	"<br>Cloud cover: " + (data.currently.cloudCover*100) + "%" +
+	"<br>Humidity: " + (data.currently.humidity*100) + "%" +
 	"<br>Temperature: " + (Math.round(((data.currently.temperature - 32) * 5 / 9)*100)/100) + "°C";
 
 	el = document.getElementById("summary");
