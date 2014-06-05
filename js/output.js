bfcb0fcdbc2a034a7a547c3b472faf6bdb875947
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2014-06-05 16:12:41 +0200

    temperature

--- js/output.js
+++ js/output.js
@@ -1,9 +1,10 @@
 
 function output(data, situation) {
 	var el = document.getElementById("weather");
-	el.innerHTML = "Summary: " + data['currently']['summary'] + 
-	"<br>Cloud cover: " + data['currently']['cloudCover'] + 
-	"<br>Humidity: " + data['currently']['humidity'];
+	el.innerHTML = "Summary: " + data.currently.summary + 
+	"<br>Cloud cover: " + data.currently.cloudCover + 
+	"<br>Humidity: " + data.currently.humidity +
+	"<br>Temperature: " + data.currently.temperature -32.0 * 5.0 / 9.0;
 
 	el = document.getElementById("summary");
 	el.innerHTML = "Conditions are<br><strong class=\"important\">" + situation + ".</strong><br>";
