1caa04e17cfe25da85d794281703ea2bdab5d65b
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2014-06-06 16:15:39 +0200

    solved ajax request -_-

--- js/output.js
+++ js/output.js
@@ -3,7 +3,7 @@ function output(data, situation) {
 	var el = document.getElementById("weather");
 	el.innerHTML = "Summary: <strong>" + data.currently.summary + "</strong>" +
 	"<br>Cloud cover: " + (Math.round(data.currently.cloudCover*100)*100)/100 + "%" +
-	"<br>Humidity: " + (Math.round((data.currently.humidity*100)*100)/100 + "%" +
+	"<br>Humidity: " + (Math.round(data.currently.humidity*100)*100)/100 + "%" +
 	"<br>Temperature: " + (Math.round(((data.currently.temperature - 32) * 5 / 9)*100)/100) + "°C";
 
 	el = document.getElementById("summary");
